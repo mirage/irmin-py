0fb58e7210d98a0fcb1b0f0337b5e7bc6f61c284
zach <zachshipko@gmail.com>
2022-02-25 09:06:26 -0800

    Make version point to which irmin version to use

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 setup(
     name="irmin",
-    version="0.1.0b0",
+    version="0.3.1b0",
     description="Irmin bindings for Python",
     author="Zach Shipko",
     author_email="zachshipko@gmail.com",
